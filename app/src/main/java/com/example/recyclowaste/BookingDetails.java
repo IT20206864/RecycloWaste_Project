10d5d17c2b2e23c3814eb469e26e8be602838a69
anjulasjay <anjulasjay@gmail.com>
2021-08-11 19:18:45 +0530

    Added BookingDetails

--- app/src/main/java/com/example/recyclowaste/BookingDetails.java
+++ app/src/main/java/com/example/recyclowaste/BookingDetails.java
@@ -2,7 +2,9 @@ package com.example.recyclowaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BookingDetails extends AppCompatActivity {
 
@@ -11,4 +13,6 @@ public class BookingDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_details);
     }
+
+
 }
--- app/src/main/java/com/example/recyclowaste/BookingPayment.java
+++ app/src/main/java/com/example/recyclowaste/BookingPayment.java
@@ -2,7 +2,9 @@ package com.example.recyclowaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BookingPayment extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class BookingPayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_payment);
+    }
+
+    public void clickConfirm(View view) {
+        Intent details = new Intent(this, BookingDetails.class);
+        startActivity(details);
     }
 }
