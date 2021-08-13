3366857b68f7e6c94abcadb344069846081b11ae
anjulasjay <anjulasjay@gmail.com>
2021-08-14 00:14:03 +0530

    some updates

--- app/src/main/java/com/example/recyclowaste/MyBookings.java
+++ app/src/main/java/com/example/recyclowaste/MyBookings.java
@@ -19,20 +19,15 @@ public class MyBookings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_bookings);
         myBookingsView = findViewById(R.id.myBookingsView);
-        Booking booking1 = new Booking("Kusal","Domestic Waste", "Malabe Town", "08-08-2021","08:30AM");
-        Booking booking2 = new Booking("Ramesh","Medical Waste", "Matale Town", "08-07-2021","09:30AM");
-        Booking booking3 = new Booking("Niroshan","Electronic Waste", "Kandy Town", "08-06-2021","10:30AM");
-        Booking booking4 = new Booking("Kusal","Domestic Waste", "Malabe Town", "08-08-2021","08:30AM");
-        Booking booking5 = new Booking("Ramesh","Medical Waste", "Matale Town", "08-07-2021","09:30AM");
-        Booking booking6 = new Booking("Niroshan","Electronic Waste", "Kandy Town", "08-06-2021","10:30AM");
+        Booking booking1 = new Booking("Sameera Silva","Domestic Waste", "Malabe Town", "08-08-2021","08:30AM");
+        Booking booking2 = new Booking("Hasitha Dulan","Medical Waste", "Kaduwela", "08-07-2021","09:30AM");
+        Booking booking3 = new Booking("Roshan Jayasena","Domestic Waste", "Malabe Town", "08-06-2021","10:30AM");
+
 
         list = new ArrayList<>();
         list.add(booking1);
         list.add(booking2);
         list.add(booking3);
-        list.add(booking4);
-        list.add(booking5);
-        list.add(booking6);
 
         myBookingsView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new Adapter(this, list);
