2646b0712f919e751ec9e3b8b77fb6268105345f
o_weerasinghe <oshanweerasinghe28@gmail.com>
2021-09-23 13:47:52 +0530

    ReviewsAdded
    FD,FB and tip

--- app/src/main/java/com/example/recyclowaste/Reviews.java
+++ app/src/main/java/com/example/recyclowaste/Reviews.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Reviews extends AppCompatActivity {
     Review review;
     TextView tv_tipView;
+    TextView nd_customamount;
     Button bt_hundred;
 
     @Override
@@ -19,14 +21,53 @@ public class Reviews extends AppCompatActivity {
         setContentView(R.layout.activity_reviews);
         review = new Review(100);
         tv_tipView = (TextView)findViewById(R.id.tv_tipView);
-
-        //tv_tipView = findViewById(R.id.nd_customamount);
-
+        nd_customamount = (EditText) findViewById(R.id.nd_customamount);
         tv_tipView.setText(review.getTip());
 
         Button submitButtonBooking = (Button) findViewById(R.id.bt_forbooking);
         Button submitButtonDriver = (Button) findViewById(R.id.bt_fordriver);
         Button submitButtonHundred = (Button) findViewById(R.id.bt_hundred);
+        Button submitButtonFifty = (Button) findViewById(R.id.bt_fifty);
+        Button submitButtonFivehundred = (Button) findViewById(R.id.bt_fhundred);
+        Button submitForm = (Button) findViewById(R.id.bt_done);
+
+        submitButtonHundred.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tip = nd_customamount.getText().toString();
+                int currenttip = 100;
+                tv_tipView.setText(currenttip+tip);
+            }
+        });
+
+        submitButtonFifty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tip = nd_customamount.getText().toString();
+                int currenttip = 50;
+                tv_tipView.setText(currenttip+tip);
+            }
+        });
+
+        submitButtonFivehundred.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tip = nd_customamount.getText().toString();
+                int currenttip = 500;
+                tv_tipView.setText(currenttip+tip);
+            }
+        });
+
+        submitForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get values from texteditor to textviewer
+                String tip = nd_customamount.getText().toString();
+                tv_tipView.setText("Tip :"+tip);
+            }
+        });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // perform click event on button booking
         submitButtonBooking.setOnClickListener(new View.OnClickListener() {
@@ -47,13 +88,8 @@ public class Reviews extends AppCompatActivity {
                 startActivity(intentd);
             }
         });
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        submitButtonHundred.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tv_tipView.setText(review.getTip());
-            }
-        });
     }
 
     public void openBookUser(View view) {
