803cde62ef6d24bb51dbdf4561586d6542418c65
Anjula Jayasinghe <83394637+IT20222222@users.noreply.github.com>
2021-09-24 16:35:52 +0530

    user profile, edit profile

--- app/src/main/java/com/example/recyclowaste/model/User.java
+++ app/src/main/java/com/example/recyclowaste/model/User.java
@@ -1,16 +1,48 @@
 package com.example.recyclowaste.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String fname;
     private String lname;
     private String email;
     private String telno;
+    private String username;
+    private String password;
+    private String type;
 
-    public User(String fname, String lname, String email, String telno) {
+    public User(String fname, String lname, String email, String telno, String username, String password, String type) {
         this.fname = fname;
         this.lname = lname;
         this.email = email;
         this.telno = telno;
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getFname() {
