371ab7623beb9505c7d3cf5ef60564d65d1d720d
Anjula Jayasinghe <83394637+IT20222222@users.noreply.github.com>
2021-10-02 19:13:37 +0530

    fixed edit profile

--- app/src/main/java/com/example/recyclowaste/EditProfile.java
+++ app/src/main/java/com/example/recyclowaste/EditProfile.java
@@ -12,6 +12,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.recyclowaste.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +33,9 @@ public class EditProfile extends AppCompatActivity {
     User user;
     DatabaseReference dbref;
     String key;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser usr;
+    AuthCredential credential;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +43,18 @@ public class EditProfile extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
         Intent intent = getIntent();
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        usr = firebaseAuth.getCurrentUser();
+
         fname = findViewById(R.id.inptFname);
         email = findViewById(R.id.inptEmail);
         telno = findViewById(R.id.inptTelno);
 
         user = (User) intent.getSerializableExtra("user");
         key = intent.getStringExtra("key");
+        credential = EmailAuthProvider
+                .getCredential(user.getEmail(), user.getPassword());
+
 
         fname.setText(user.getFname());
         email.setText(user.getEmail());
@@ -59,11 +74,27 @@ public class EditProfile extends AppCompatActivity {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     if(snapshot.hasChildren()){
-                        dbref.child("fname").setValue(user.getFname());
-                        dbref.child("email").setValue(user.getEmail());
-                        dbref.child("telno").setValue(user.getTelno());
+                        usr.reauthenticate(credential)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        FirebaseUser usr = FirebaseAuth.getInstance().getCurrentUser();
+                                        usr.updateEmail(user.getEmail())
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            dbref.child("fname").setValue(user.getFname());
+                                                            dbref.child("email").setValue(user.getEmail());
+                                                            dbref.child("telno").setValue(user.getTelno());
 
-                        Toast.makeText(getApplicationContext(), "Successfull!", Toast.LENGTH_SHORT).show();
+                                                            Toast.makeText(getApplicationContext(), "Successfull!", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
+                                    }
+                                });
+
                     }
                     else {
                         Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
